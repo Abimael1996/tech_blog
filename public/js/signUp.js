02e251025f433709ba588bc4bda755755e8adcea
abi <abimael.monarrez58@gmail.com>
2022-02-22 11:08:23 -0700

    post request to create new user done

--- public/js/signUp.js
+++ public/js/signUp.js
@@ -1,5 +1,31 @@
 const username = document.querySelector("#username");
 const password = document.querySelector("#password");
+const signUpBtn = document.querySelector("#signUpBtn");
 
-console.log(username);
-console.log(password);
+console.log(signUpBtn);
+
+const createNewUser = async (event) => {
+    event.preventDefault();
+
+    const newUser = {
+        username: username.value,
+        password: password.value
+    }
+
+    try {
+        const response = await fetch("api/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "Application/JSON"
+            },
+            body: JSON.stringify(newUser)
+        });
+        if(response.ok) {
+            window.location.replace("/")
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+signUpBtn.addEventListener("click", createNewUser);
