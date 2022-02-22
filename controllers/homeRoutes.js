0506ec844587a74ac28ebaea26ce7c3478a30c20
abi <abimael.monarrez58@gmail.com>
2022-02-21 18:56:56 -0700

    get request to login page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -6,5 +6,13 @@ router.get("/", async (req, res) => {
     } catch (err) {
         res.status(400).json(err);
     }
+});
+
+router.get("/login", async (req, res) => {
+    try {
+        res.render("login")
+    } catch (err) {
+        res.status(400).json(err);
+    }
 })
 module.exports = router;
